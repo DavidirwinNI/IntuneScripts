627b74097a7ca5d91b733a5b953b4f2c360a89ef
David Irwin <davidirwin77@gmail.com>
2023-10-01 23:36:01 +0100

    Add files via upload

---
+++ Teams_company_detection_script.ps1
@@ -0,0 +1,4 @@
+﻿If([String](Get-Item -Path "$Env:ProgramFiles\Teams Installer\Teams.exe","${Env:ProgramFiles(x86)}\Teams Installer\Teams.exe" -ErrorAction SilentlyContinue).VersionInfo.FileVersion -ge "1"){
+write-host (Get-Command "C:\Program Files (x86)\Teams Installer\Teams.exe").FileVersionInfo.FileVersion 
+exit 1
+}
